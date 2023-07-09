2b9e1f1ef91e000fa149bc91b65bd4613d4fb821
Anwesh43 <anweshthecool0@gmail.com>
2023-07-09 19:15:42 +0530

    adding Animator Helper and refactoring some portions of code

--- public/components.js
+++ public/components.js
@@ -1,31 +1,73 @@
 var inputBox = document.getElementById('textInput');
 var button = document.getElementById('submit');
 var output = document.getElementById('output');
+var DELAY = 10;
+var Loop = /** @class */ (function () {
+    function Loop() {
+        this.animated = false;
+        this.cbs = [];
+        this.count = 0;
+    }
+    Loop.prototype.push = function (cb) {
+        this.count++;
+        this.cbs.push({
+            id: this.count,
+            cb: cb
+        });
+        return this.count;
+    };
+    Loop.prototype.start = function () {
+        var _this = this;
+        if (!this.animated) {
+            this.animated = true;
+            this.interval = setInterval(function () {
+                _this.cbs.forEach(function (an) {
+                    an.cb();
+                });
+            }, DELAY);
+        }
+    };
+    Loop.prototype.stop = function (id) {
+        this.cbs = this.cbs.filter(function (an) { return an.id != id; });
+        if (this.cbs.length == 0) {
+            this.animated = false;
+            clearInterval(this.interval);
+        }
+    };
+    return Loop;
+}());
 var Stage = /** @class */ (function () {
     function Stage(image, quote) {
         this.image = image;
         this.quote = quote;
+        this.context = null;
+        this.canvas = document.createElement('canvas');
     }
+    Stage.prototype.initCanvas = function () {
+        this.canvas = document.createElement('canvas');
+        this.canvas.width = 500;
+        this.canvas.height = 300;
+        this.context = this.canvas.getContext('2d');
+        output.appendChild(this.canvas);
+    };
     Stage.prototype.render = function () {
         var _this = this;
-        var canvas = document.createElement('canvas');
-        canvas.width = 500;
-        canvas.height = 300;
-        var context = canvas.getContext('2d');
-        output.appendChild(canvas);
+        var w = this.canvas.width;
+        var h = this.canvas.height;
         var img = new Image(256, 256);
         img.src = this.image;
         img.onload = function () {
-            if (context) {
+            if (_this.context) {
+                var context = _this.context;
                 context.fillStyle = '#212121';
-                context.fillRect(0, 0, canvas.width, canvas.height);
-                var x = canvas.width / 2 - img.width / 2;
-                var y = canvas.height / 2 - img.height / 2;
+                context.fillRect(0, 0, w, h);
+                var x = w / 2 - img.width / 2;
+                var y = h / 2 - img.height / 2;
                 context.drawImage(img, x, y);
                 context.font = context.font.replace(/\d+/, "16");
                 console.log(context.font);
                 context.fillStyle = '#BDBDBD';
-                context.fillText(_this.quote, canvas.width / 2 - context.measureText(_this.quote).width / 2, y + img.height + 20);
+                context.fillText(_this.quote, w / 2 - context.measureText(_this.quote).width / 2, y + img.height + 20);
             }
         };
     };
--- public/components.ts
+++ public/components.ts
@@ -4,31 +4,91 @@ const button : HTMLButtonElement = document.getElementById('submit') as HTMLButt
 const output : HTMLDivElement = document.getElementById('output') as HTMLDivElement 
 
 type ImageType = typeof Image 
+
+type IntervalType = ReturnType<typeof setInterval>
+
+const DELAY : number = 10 
+
+type AnimationFn = () => void
+
+interface AnimationNode {
+    id : number, 
+    cb : AnimationFn
+}
+class Loop {
+
+    animated : boolean = false 
+    interval? : IntervalType
+
+    cbs : Array<AnimationNode> = []
+    count : number = 0
+
+    push(cb : AnimationFn) : number  {
+        this.count++
+        this.cbs.push({
+            id: this.count, 
+            cb
+        })
+        return this.count
+    }
+
+    start() {
+        if (!this.animated) {
+            this.animated = true 
+            this.interval = setInterval(() => {
+                this.cbs.forEach((an : AnimationNode) => {
+                    an.cb()
+                })
+            }, DELAY)
+        }
+    }
+
+    stop(id : number) {
+        this.cbs = this.cbs.filter((an : AnimationNode) => an.id != id)
+        if (this.cbs.length == 0) {
+            this.animated = false 
+            clearInterval(this.interval)
+        }
+    }
+
+}
+
+
 class Stage {
     
+    context : CanvasRenderingContext2D | null = null 
+    canvas : HTMLCanvasElement = document.createElement('canvas')
     constructor(private image : string, private quote : string) {
     
     }
 
+    initCanvas() {
+        this.canvas = document.createElement('canvas')
+        this.canvas.width = 500 
+        this.canvas.height = 300 
+        this.context = this.canvas.getContext('2d')
+        output.appendChild(this.canvas)
+    }
+
     render() {
-        const canvas = document.createElement('canvas')
-        canvas.width = 500 
-        canvas.height = 300 
-        const context = canvas.getContext('2d')
-        output.appendChild(canvas)
+        
+        const w : number = this.canvas.width 
+        const h : number = this.canvas.height 
+
         const img = new Image(256, 256)
         img.src = this.image 
         img.onload = () => {
-            if (context) {
+            if (this.context) {
+                const context : CanvasRenderingContext2D = this.context 
                 context.fillStyle = '#212121'
-                context.fillRect(0, 0, canvas.width, canvas.height)
-                const x = canvas.width / 2 - img.width / 2
-                const y = canvas.height / 2 - img.height / 2
+                context.fillRect(0, 0, w, h)
+                const x = w / 2 - img.width / 2
+                const y = h / 2 - img.height / 2
                 context.drawImage(img, x, y)
                 context.font = context.font.replace(/\d+/, `16`)
                 console.log(context.font)
                 context.fillStyle = '#BDBDBD'
-                context.fillText(this.quote, canvas.width / 2 - context.measureText(this.quote).width / 2, y +img.height + 20)
+                context.fillText(this.quote, w / 2 - context.measureText(this.quote).width / 2, y +img.height + 20)
             }
             
         }
